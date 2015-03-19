cb13909c0e07739d5abfc6b1d06a4c0cced3018a
Paul Springett <paul.springett@songkick.com>
2015-03-19 10:37:19 +0000

    Add dummy "environment" ruby file

---
+++ examples/environment.rb
@@ -0,0 +1,18 @@
+require_relative '../lib/songkick/consumer'
+require 'logger'
+
+Songkick::Consumer.configure do |config|
+  config.amqp = 'amqp://localhost:5672'
+  config.logger = Logger.new(STDOUT)
+end
+
+class TweetConsumer
+  # This should be defined be calling consumer_from_queue (see README for example)
+  def self.queue_name
+    'tweets-queue'
+  end
+
+  def process(payload)
+    puts "TweetConsumer#process(#{payload})"
+  end
+end
