7bb9437912a9f233d5d89e0cdcc5cfa264cb3e6a
Paul Springett <paul.springett@songkick.com>
2015-03-31 11:39:48 +0100

    Simplify documentation for SongkickQueue.publish

--- lib/songkick_queue.rb
+++ lib/songkick_queue.rb
@@ -29,10 +29,9 @@ module SongkickQueue
 
   # Publishes the given message to the given queue
   #
-  # @param queue_name [String] to publish to
-  # @param message [#to_json] to serialize and enqueue
-  def self.publish(queue_name, message)
-    producer.publish(queue_name, message)
+  # @see SongkickQueue::Producer#publish for argument documentation
+  def self.publish(queue_name, message, options = {})
+    producer.publish(queue_name, message, options = {})
   end
 
   private
