aafbec4c146faa2cba69023531a7775e8b9a1677
Paul Springett <paul.springett@songkick.com>
2015-05-01 17:51:16 +0100

    Remove `queue_namespace` config option
    RabbitMQ has virtual hosts do to proper namespacing for exchanges and queues

--- lib/songkick_queue/consumer.rb
+++ lib/songkick_queue/consumer.rb
@@ -16,8 +16,6 @@ module SongkickQueue
       def queue_name
         @queue_name or fail(NotImplementedError, 'you must declare a queue name to consume from ' +
           'by calling #consume_from_queue in your consumer class. See README for more info.')
-
-        [config.queue_namespace, @queue_name].compact.join('.')
       end
 
       def config
