fdbfbfea114b2d0861c5a6f2e8d1eed5c943bf43
Paul Springett <paul.springett@songkick.com>
2015-05-18 14:45:53 +0100

    Extract building of client connections and channels into separate methods

--- lib/songkick_queue/client.rb
+++ lib/songkick_queue/client.rb
@@ -8,35 +8,42 @@ module SongkickQueue
     #
     # @return [Bunny::Channel]
     def channel
-      @channel ||= begin
-        channel = connection.create_channel
-        channel.prefetch(1)
-
-        channel
-      end
+      @channel ||= build_channel
     end
 
     # Creates a memoized connection to RabbitMQ
     #
     # @return [Bunny::Session]
     def connection
-      @connection ||= begin
-        connection = Bunny.new(
-          host: config.host,
-          port: config.port,
-          username: config.username,
-          password: config.password,
-          vhost: config.vhost,
-          heartbeat_interval: 60,
-        )
-
-        connection.start
-
-        connection
-      end
+      @connection ||= build_connection
     end
 
     private
+
+    def build_channel
+      channel = connection.create_channel
+      channel.prefetch(1)
+
+      channel
+    end
+
+    def build_connection
+      connection = Bunny.new(
+        host: config.host,
+        port: config.port,
+        username: config.username,
+        password: config.password,
+        vhost: config.vhost,
+        heartbeat_interval: 10,
+        automatically_recover: true,
+        network_recovery_interval: config.network_recovery_interval,
+        recover_from_connection_close: true,
+      )
+
+      connection.start
+
+      connection
+    end
 
     def config
       SongkickQueue.configuration
